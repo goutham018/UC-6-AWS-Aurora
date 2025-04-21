52413176355356cf1bab9eba50c55b8085605107
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-04-21 10:37:29 +0530

    Updated variable.tf

--- variable.tf
+++ variable.tf
@@ -67,7 +67,7 @@ variable "aurora_cluster_name" {
 variable "secrets_manager_secret_name" {
   type        = string
   description = "The name of the secret in AWS Secrets Manager"
-  default     = "aurora-master-credentials-v8" # Use a new unique name
+  default     = "aurora-master-credentials-v9" # Use a new unique name
 }
 
 variable "db_port" {
